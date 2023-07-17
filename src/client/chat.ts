e63bc6f2f5befd484f19804dbec9a00b7873df82
kage <81172243+kage95@users.noreply.github.com>
2023-07-17 20:27:13 +0900

    fix path

--- src/client/chat.ts
+++ src/client/chat.ts
@@ -1,7 +1,8 @@
 import * as dotenv from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
+const path = require("path");
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const configuration = new Configuration({
   organization: process.env.ORGANIZATION,

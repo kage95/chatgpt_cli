ed11120158aef4e9142714a14438ed2665130805
kage <81172243+kage95@users.noreply.github.com>
2023-07-17 17:36:53 +0900

    format

--- src/commands/chat/index.ts
+++ src/commands/chat/index.ts
@@ -1,14 +1,17 @@
 import { Command, ux } from "@oclif/core";
+import { HelpFormatter } from "@oclif/core/lib/help/formatter";
 import * as notifier from "node-notifier";
 import chat from "../../client/chat";
 
 export default class Chat extends Command {
   async run(): Promise<void> {
-    const prompt = await ux.prompt("provide prompt");
+    const prompt = await ux.prompt("お、質問か？");
+    const formatter = new HelpFormatter(this.config, { maxWidth: 128 });
+    formatter.indentSpacing = 10;
 
-    ux.action.start("generating...");
+    ux.action.start("ちょっと待ってや〜〜");
     const completion = await chat.createChatCompletion({
-      model: "gpt-3.5-turbo",
+      model: "gpt-4",
       messages: [
         {
           role: "system",
@@ -17,13 +20,18 @@ export default class Chat extends Command {
         { role: "user", content: `${prompt}` },
       ],
     });
-    ux.action.stop();
+    ux.action.stop("\n");
 
     notifier.notify({
-      title: "Finish!!",
-      message: "Message generation has been completed",
+      title: "おーい",
+      message: "できましたでぇ〜〜",
     });
 
-    this.log(`${completion.data.choices[0].message?.content}`);
+    this.log(
+      formatter.indent(
+        formatter.wrap(`${completion.data.choices[0].message?.content}`)
+      )
+    );
+    this.exit();
   }
 }

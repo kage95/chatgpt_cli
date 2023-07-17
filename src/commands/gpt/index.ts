920e03b7caddd3eef8f60ef52cf6d4faccfcf568
kage <81172243+kage95@users.noreply.github.com>
2023-07-17 15:10:40 +0900

    add spinner

--- src/commands/gpt/index.ts
+++ src/commands/gpt/index.ts
@@ -1,22 +1,27 @@
-import { Args, Command } from "@oclif/core";
+import { Command, ux } from "@oclif/core";
+import * as notifier from "node-notifier";
 import gpt from "../../client/gpt";
 
 export default class Gpt extends Command {
-  static flags = {};
-
-  static args = {
-    prompt: Args.string({
-      description: "provide prompt",
-      required: true,
-    }),
-  };
-
   async run(): Promise<void> {
-    const { args } = await this.parse(Gpt);
+    const prompt = await ux.prompt("provide prompt");
 
+    ux.action.start("generating...");
     const completion = await gpt.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: `${args.prompt}` }],
+      messages: [
+        {
+          role: "system",
+          content: "関西弁で話してください。",
+        },
+        { role: "user", content: `${prompt}` },
+      ],
+    });
+    ux.action.stop();
+
+    notifier.notify({
+      title: "Finish!!",
+      message: "Message generation has been completed",
     });
 
     this.log(`${completion.data.choices[0].message?.content}`);
